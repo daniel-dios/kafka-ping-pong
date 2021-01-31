c7167ab093334c8043df3093d060dc99a858971d
daniel-dios <daniel.schezdios@gmail.com>
2021-01-31 23:01:36 +0100

    disabled kafka helper test

--- src/test/java/com/kafkapingpong/framework/helper/kafka/KafkaTest.java
+++ src/test/java/com/kafkapingpong/framework/helper/kafka/KafkaTest.java
@@ -1,48 +1,37 @@
 package com.kafkapingpong.framework.helper.kafka;
 
 import com.kafkapingpong.framework.helper.DockerComposeHelper;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.Duration;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DirtiesContext
 class KafkaTest {
 
   private static final String MESSAGE_CONTENT = """
       {"message": "content"}
               """;
-  private static final String LOCALHOST = "localhost";
-  private static final int PORT = 9094;
   private static final String TOPIC = "ping";
-  private static final DockerComposeHelper dockerComposeHelper =
-      new DockerComposeHelper(DockerComposeHelper.Compose.KAFKA);
 
-  @BeforeAll
-  static void beforeAll() {
-    dockerComposeHelper.start();
-  }
-
-  @AfterAll
-  static void afterAll() {
-    dockerComposeHelper.stop();
-  }
-
+  @Disabled
   @Test
   void shouldConsumeMessage() throws Exception {
+    final var dockerComposeHelper = new DockerComposeHelper(DockerComposeHelper.Compose.KAFKA);
+    dockerComposeHelper.start();
     final var kafkaConsumerHelper = new KafkaConsumerHelper(Collections.singletonList(TOPIC));
     kafkaConsumerHelper.consumeAll();
 
     new KafkaProducerHelper().send(TOPIC, MESSAGE_CONTENT);
+
     final var records = kafkaConsumerHelper.consumeAtLeast(1, Duration.ofSeconds(1));
     final var all = records.findAll();
 
     assertThat(all).hasSize(1);
     assertThat(all.get(0).value()).isEqualTo(MESSAGE_CONTENT);
+
+    dockerComposeHelper.stop();
   }
 }
