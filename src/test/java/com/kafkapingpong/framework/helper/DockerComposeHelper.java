772ff0ef56c5e585ebd03e419bca00e324778495
daniel-dios <daniel.schezdios@gmail.com>
2021-02-04 10:03:20 +0100

    fixed test

--- src/test/java/com/kafkapingpong/framework/helper/DockerComposeHelper.java
+++ src/test/java/com/kafkapingpong/framework/helper/DockerComposeHelper.java
@@ -36,17 +36,15 @@ public class DockerComposeHelper extends DockerComposeContainer<DockerComposeHel
                 .withStrategy(forListeningPort())
                 .withStrategy(forLogMessage(".*database system is ready to accept connections.*", 1)))
         .withExposedService(KAFKA, KAFKA_PORT)
-        .waitingFor(
-            KAFKA,
-            new WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-                .withStrategy(forListeningPort())
-                .withStrategy(forLogMessage(".*Created topic*", 3)))
+        .waitingFor(KAFKA, new WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+            .withStrategy(forListeningPort())
+            .withStrategy(forLogMessage(".*creating topics.*", 1))
+        )
         .withExposedService(ZOOKEEPER, ZOOKEEPER_PORT)
-        .waitingFor(
-            ZOOKEEPER,
-            new WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-                .withStrategy(forListeningPort())
-                .withStrategy(forLogMessage(".*binding to port.*", 1)));
+        .waitingFor(ZOOKEEPER, new WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+            .withStrategy(forListeningPort())
+            .withStrategy(forLogMessage(".*binding to port.*", 1))
+        );
   }
 
   @Override
